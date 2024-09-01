cd284a38208121813dd0c9ff4814873ae212f15d
Aleks-Daniel Jakimenko-Aleksejev <alex.jakimenko@gmail.com>
2024-09-02 02:07:37 +0300

    Update the about page
    There's not much to write about it, but some links don't hurt at all.

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
-	//
+	import { Button, Icon } from 'svelte-ux';
+	import { mdiHeart } from '@mdi/js';
 </script>
 
 <main class="p-2">
 	<h1 class="text-3xl font-semibold">About</h1>
-	<p>Track your mood throughout the years!</p>
+	<p class="my-2">Track your mood throughout the years!</p>
 
-	<p>
-		Built with <a href="https://svelte.dev/" target="_blank">Svelte</a>
-		and <a href="https://svelte-ux.techniq.dev/">Svelte UX</a>.
+	<p class="my-2">
+		Built with
+		<Button target="_blank" color="primary" class="p-0 py-0.5" href="https://svelte.dev/"
+			>Svelte</Button
+		>,
+		<Button target="_blank" color="primary" class="p-0 py-0.5" href="https://kit.svelte.dev/"
+			>SvelteKit</Button
+		>,
+		<Button target="_blank" color="primary" class="p-0 py-0.5" href="https://svelte-ux.techniq.dev/"
+			>Svelte UX</Button
+		>,
+		<Button target="_blank" color="primary" class="p-0 py-0.5" href="https://hono.dev/">Hono</Button
+		> and
+		<Button target="_blank" color="primary" class="p-0 py-0.5" href="https://orm.drizzle.team/"
+			>Drizzle ORM</Button
+		>.
+	</p>
+
+	<p class="my-2">
+		Made with <Icon data={mdiHeart} class="align-bottom text-red-400" size="1.35em" />
+		by <Button
+			href="https://github.com/AlexDaniel"
+			target="_blank"
+			color="primary"
+			class="p-0 py-0.5">AlexDaniel</Button
+		>
 	</p>
 </main>
