1f1d6cac9cc6f59ec701e85aa03ea82fa4ba8cd5
Aleks-Daniel Jakimenko-Aleksejev <alex.jakimenko@gmail.com>
2024-10-23 02:28:56 +0300

    Keep everything lowercase for style

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -29,7 +29,7 @@
 	<svelte:fragment slot="menuIcon">
 		<NavItem
 			path="/"
-			text="Home"
+			text="home"
 			icon="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z"
 			currentUrl={$page.url}
 			class="pl-3 pr-4 text-gray-900 [&:where(.is-active)]:text-sky-100"
@@ -37,7 +37,7 @@
 
 		<NavItem
 			path="/about"
-			text="About"
+			text="about"
 			icon="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z"
 			currentUrl={$page.url}
 			class="pl-3 pr-4 text-gray-900 [&:where(.is-active)]:text-sky-100"
